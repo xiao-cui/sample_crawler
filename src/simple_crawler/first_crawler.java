4c8d69113edefbbb77c36dd5e88966753d7bb976
xiao-cui <cxstam@gmail.com>
2014-06-15 00:22:05 +1000

    爬取知乎编辑推荐页面下所有的标题（使用arraylist储存） only titles on current_page 通过审查页面结构meta数据，确定encoding=UTF-8, define encoding in bufferedreader

--- src/simple_crawler/first_crawler.java
+++ src/simple_crawler/first_crawler.java
@@ -4,6 +4,8 @@ package simple_crawler;
 
 import java.io.*;
 import java.net.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,7 +25,7 @@ public class first_crawler{
 			//start connect
 			connection.connect();
 			//initiate bufferedreader
-			in=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			in=new BufferedReader(new InputStreamReader(connection.getInputStream(),"UTF-8"));
 			//define a variable storing web content temporarily line by line
 			String line;
 			while ((line=in.readLine())!=null) {
@@ -46,16 +48,21 @@ public class first_crawler{
 		return result;
 	}
 
-	static String RegexString(String targetStr, String patternStr) {
+	static ArrayList<String> RegexString(String targetStr, String patternStr) {
+		//use arraylist to store matched sub-string
+		ArrayList<String> results=new ArrayList<String>();
 		//define a pattern
 		Pattern pattern=Pattern.compile(patternStr);
 		//define a matcher
 		Matcher matcher=pattern.matcher(targetStr);
 		//if found
-		if(matcher.find()){
-			return matcher.group(1);
+		boolean isFind=matcher.find();
+		while(isFind){
+			results.add(matcher.group(1));
+			//move to next matched sub-string
+			isFind=matcher.find();
 		}
-		return "Nothing";
+		return results;
 	}
 	
 	public static void main(String[] args) {
@@ -64,7 +71,7 @@ public class first_crawler{
 		//visit the specified url and retrieve web content
 		String result=sendGet(url);
 		//use pattern to find src content
-		String imgSrc=RegexString(result, "question_link.+?>(.+?)<");
+		ArrayList<String> imgSrc=RegexString(result, "question_link.+?>(.+?)<");
 		System.out.println(imgSrc);
 	}
 
